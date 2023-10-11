7a42a0fc8099b3aeae8caacd6f096b57cf034baf
teoma145 <matteodark145@yahoo.it>
2023-10-11 16:26:28 +0200

    only1cicle

--- java/javascript.js
+++ java/javascript.js
@@ -1,6 +1,6 @@
 const containerel = document.getElementById('wrapper')
 
-for(let i =1 ; i < 11 ; i++ ){
+/*for(let i =1 ; i < 11 ; i++ ){
     for(let c = 1 ; c < 11 ; c++){
         let number = c + (i - 1) * 10 ;
         let scritta
@@ -27,4 +27,32 @@ for(let i =1 ; i < 11 ; i++ ){
         boxEl.innerHTML = scritta;
         containerel.append(boxEl);
     }
+}*/
+
+//rifatta con un solo ciclo for
+for(let number =1 ; number  < 101; number++ ){
+        
+        let scritta
+        let color;
+        if( number % 3 === 0 && number % 5 === 0){
+            color = 'fizzbuzzcol';
+            scritta = `fizzbuzz`
+            
+        }
+        else if( number % 3 === 0){
+            color = 'fizzcol'
+            scritta = `Fizz`
+        }
+        else if (number % 5 === 0){
+            color = 'buzzcol'
+            scritta= `buzz`
+        }
+        else{
+            color = 'numbers'
+            scritta = number
+        }
+        const boxEl = document.createElement('div');
+        boxEl.className = `box ${color} text-center `
+        boxEl.innerHTML = scritta;
+        containerel.append(boxEl);
 }
